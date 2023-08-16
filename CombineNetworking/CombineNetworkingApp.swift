7830d26afb8560f07dd95c83d2b6cc08e840e023
Mohammad Murtuza <murtuza070886@gmail.com>
2023-08-16 14:48:27 +0530

    First commit

---
+++ CombineNetworking/CombineNetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineNetworkingApp.swift
+//  CombineNetworking
+//
+//  Created by Abdul Fattah on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineNetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
